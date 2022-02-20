ba5e7d803f21f521cfef975a3fcb3690417251f1
AlyPajon <alejandropajon098@gmail.com>
2022-02-20 17:27:55 +0100

    es

--- AlitaAngelDeBatalla.js
+++ AlitaAngelDeBatalla.js
@@ -1,3 +1,5 @@
 console.log('Alita: angel de batalla', "es la ostia tio","omegalol");
 // La mejor adaptación de un manga
 console.log('Alita: angel de batalla', "es la ostia tio");
+
+console.log("era hace una vez *ce desmaya* FIN");
